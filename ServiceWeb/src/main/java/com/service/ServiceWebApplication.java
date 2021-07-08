2122dc21265933b4daecd429d8c3f27c228cc27c
yifujun <2577231604@qq.com>
2021-07-08 21:21:23 +0800

    111

--- ServiceWeb/src/main/java/com/service/ServiceWebApplication.java
+++ ServiceWeb/src/main/java/com/service/ServiceWebApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServiceWebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceWebApplication.class, args);
+        SpringApplication.run(ServiceWebApplication.class,  args);
     }
 
 }
